2c1876e3cd57817073c64fc4da446607bcf9e17a
Stefan Lewicki <stefan.lewicki@ms.bulme.at>
2019-02-08 22:26:22 +0100

    Version 1.0 Picks Random Files of Source Folder and Drops them into the Target Folder. Chance of Image Picked can be Choosen

--- RandomImagePicker/Form1.Designer.cs
+++ RandomImagePicker/Form1.Designer.cs
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.btn_target = new System.Windows.Forms.Button();
+            this.lbl_source = new System.Windows.Forms.Label();
+            this.lbl_target = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.txb_chance = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quelle";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ziel";
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(12, 29);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(75, 23);
+            this.btn_select.TabIndex = 2;
+            this.btn_select.Text = "Select";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // btn_target
+            // 
+            this.btn_target.Location = new System.Drawing.Point(145, 29);
+            this.btn_target.Name = "btn_target";
+            this.btn_target.Size = new System.Drawing.Size(75, 23);
+            this.btn_target.TabIndex = 3;
+            this.btn_target.Text = "Select";
+            this.btn_target.UseVisualStyleBackColor = true;
+            this.btn_target.Click += new System.EventHandler(this.btn_target_Click);
+            // 
+            // lbl_source
+            // 
+            this.lbl_source.AutoSize = true;
+            this.lbl_source.Location = new System.Drawing.Point(251, 13);
+            this.lbl_source.Name = "lbl_source";
+            this.lbl_source.Size = new System.Drawing.Size(37, 13);
+            this.lbl_source.TabIndex = 4;
+            this.lbl_source.Text = "Quelle";
+            // 
+            // lbl_target
+            // 
+            this.lbl_target.AutoSize = true;
+            this.lbl_target.Location = new System.Drawing.Point(251, 34);
+            this.lbl_target.Name = "lbl_target";
+            this.lbl_target.Size = new System.Drawing.Size(24, 13);
+            this.lbl_target.TabIndex = 5;
+            this.lbl_target.Text = "Ziel";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Enabled = false;
+            this.btn_start.Location = new System.Drawing.Point(12, 105);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(607, 42);
+            this.btn_start.TabIndex = 6;
+            this.btn_start.Text = "GO";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // lbl_chance
+            // 
+            this.txb_chance.Location = new System.Drawing.Point(75, 79);
+            this.txb_chance.Name = "lbl_chance";
+            this.txb_chance.Size = new System.Drawing.Size(100, 20);
+            this.txb_chance.TabIndex = 7;
+            this.txb_chance.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Chance 1:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(690, 155);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txb_chance);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.lbl_target);
+            this.Controls.Add(this.lbl_source);
+            this.Controls.Add(this.btn_target);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.Button btn_target;
+        private System.Windows.Forms.Label lbl_source;
+        private System.Windows.Forms.Label lbl_target;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.TextBox txb_chance;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- RandomImagePicker/Form1.cs
+++ RandomImagePicker/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,65 @@ namespace RandomImagePicker
 {
     public partial class Form1 : Form
     {
+        string source = null;
+        string target = null;
+        Random rand = new Random(999);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog sourceDialog = new FolderBrowserDialog())
+            {
+                if (sourceDialog.ShowDialog() == DialogResult.OK)
+                {
+                    source = sourceDialog.SelectedPath;
+                    lbl_source.Text = source;
+                }
+            }
+            if (target != null && source != null)
+                btn_start.Enabled = true;
+        }
+
+        private void btn_target_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog targetDialog = new FolderBrowserDialog())
+            {
+                if (targetDialog.ShowDialog() == DialogResult.OK)
+                {
+                    target = targetDialog.SelectedPath;
+                    lbl_target.Text = target;
+                }
+            }
+            if (target != null && source != null)
+                btn_start.Enabled = true;
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            int chance = 10;
+            try
+            {
+                chance = Convert.ToInt32(txb_chance.Text);
+            }
+            catch
+            {
+                return;
+            }
+            var files = Directory.GetFiles(source, "*.*");
+            foreach (string s in files)
+            {
+                if (rand.Next(0, chance).Equals(1))
+                {
+                    var fileName = System.IO.Path.GetFileName(s);
+                    var destFile = System.IO.Path.Combine(target, fileName);
+                    System.IO.File.Copy(s, destFile, true);
+                }
+            }
+            MessageBox.Show("Finished");
+
         }
     }
 }
